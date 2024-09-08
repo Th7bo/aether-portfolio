34fed9f24ca3df1cb506543188f882ee4eb53aa0
zen <thibo.franssen9@gmail.com>
2024-09-08 14:24:28 +0200

    fix favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,4 @@
 import { defineConfig } from "astro/config";
 
-import svelte from "@astrojs/svelte";
-
-import vercel from "@astrojs/vercel";
-
 // https://astro.build/config
-export default defineConfig({
-  integrations: [svelte()],
-  output: "server",
-  adapter: vercel(),
-});
+export default defineConfig({})
